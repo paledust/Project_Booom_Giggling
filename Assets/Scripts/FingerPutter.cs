99f3d060f9c7ffda43418617386e40f2080eb02d
paledust <paledust@hotmail.com>
2022-07-26 11:05:27 +0800

    添加基本的放手指的script

--- Assets/Scripts/FingerPutter.cs
+++ Assets/Scripts/FingerPutter.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class FingerPutter : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer fingerPrint;
-    [SerializeField] private KeyCode key;
+    [SerializeField] private Key key;
     private Color initcolor;
     void Awake(){
         initcolor = fingerPrint.color;
     }
     void Update(){
-        if(Input.GetKeyDown(key)){
+        if(Keyboard.current[key].wasPressedThisFrame){
             fingerPrint.color = Color.red;
         }
-        if(Input.GetKeyUp(key)){
+        else if(Keyboard.current[key].wasReleasedThisFrame){
             fingerPrint.color = initcolor;
         }
     }
