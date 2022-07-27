538e4e1a469440313df6e011e76d31472d2cd22e
paledust <paledust@hotmail.com>
2022-07-27 12:52:47 +0800

    修改tear paper代码，让撕扯的方向可以更多

--- Assets/Scripts/TearPaperPoint.cs
+++ Assets/Scripts/TearPaperPoint.cs
@@ -19,13 +19,11 @@ public class TearPaperPoint : MonoBehaviour
     private Vector3 endPos;
     private Vector3 initPosToPaper;
     private Vector3 tearPos;
-    private Quaternion tearpaper_initRot;
     private float offset;
     bool initialized;
     public static int AngleID = Shader.PropertyToID("_FoldAngle");
     void Awake(){
         tearPos = initPos = transform.position;
-        tearpaper_initRot = paper_tear.transform.rotation;
         tearDir = transform.right.normalized;
         tearNormalDir = transform.up.normalized;
         endPos = transform.position + transform.right * TearLength;
@@ -46,24 +44,22 @@ public class TearPaperPoint : MonoBehaviour
     }
     public void MoveTheTearPoint(Vector2 inputDirection){
         float T = Vector2.Dot(inputDirection, tearDir);
-        float N = Vector3.Dot(inputDirection, tearNormalDir);
-        offset += N * Time.deltaTime;
-        offset = Mathf.Clamp(offset, -MaxOffset, MaxOffset);
+        // float N = Vector3.Dot(inputDirection, tearNormalDir);
+        // offset += N * Time.deltaTime;
+        // offset = Mathf.Clamp(offset, -MaxOffset, MaxOffset);
 
         tearPos += Mathf.Max(0, T) * tearDir * Time.deltaTime;
-        transform.position = tearPos + offset * tearNormalDir;
+        transform.position = tearPos;
 
         Vector3 foldPoint = (initPos + transform.position)/2f;
 
-        float angle = Vector2.SignedAngle(tearDir, transform.position - initPos);
-        angle = Mathf.Clamp(angle, -5, 5);
-
+        float angle = Vector2.SignedAngle(tearDir, Vector2.right);
         Vector3 tear_fold_diff = paper_tear.transform.position - foldPoint;
         paper_tear.transform.position = tearPos + initPosToPaper;
-        paper_tear.transform.rotation = Quaternion.Euler(0, 0, angle) * tearpaper_initRot;
+        paper_tear.transform.rotation = Quaternion.Euler(0, 0, 90+transform.eulerAngles.z);
 
-        paper_tear.material.SetFloat(AngleID, -angle);
-        paper_stay.material.SetFloat(AngleID, -angle);
+        paper_tear.material.SetFloat(AngleID, 270+angle);
+        paper_stay.material.SetFloat(AngleID, 270+angle);
         Shader.SetGlobalVector(Service.DRAG_POINT_ID, new Vector4(foldPoint.x, foldPoint.y, foldPoint.z, 1));
 
         Vector3 diff = endPos - transform.position;
