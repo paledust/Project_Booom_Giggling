00dd626832a330acf34d821a2dba402eab463421
tglm <mnillx@outlook.com>
2022-08-01 22:42:10 +0800

    righthandctrl merge

--- Assets/Scripts/RighthandController.cs
+++ Assets/Scripts/RighthandController.cs
@@ -8,6 +8,9 @@ public class RighthandController : MonoBehaviour
     Vector3 initialPosition;
     Quaternion lastRot;
     Vector3 lastPos;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    private int handState = 0; 
     // Start is called before the first frame update
     void Start()
     {
@@ -25,30 +28,20 @@ public class RighthandController : MonoBehaviour
         Vector3 pos = GameManager.mainCam.WorldToScreenPoint(transform.position);
         Vector3 m_MousePos = new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, pos.z);
         transform.position = GameManager.mainCam.ScreenToWorldPoint(m_MousePos);
-
-        // lastRot = transform.rotation;
-        // lastPos = transform.position;
-        // Debug.Log(transform.position.x > lastPos.x);
-
-        // if(transform.position.x > lastPos.x)
-        // {
-        //     transform.rotation = Quaternion.Euler(0,0,lastRot.z+10);
-        // }else
-        // {
-        //     transform.rotation = Quaternion.Euler(0,0,lastRot.z-10);
-        // }
-
-
-
-
-
+    }
+    
+    void HandStateChange(){
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            if (handState == 0) {
+                handState = 1;
+                renderer.sprite = this.sprite1;
+            } else {
+                handState = 0;
+                renderer.sprite = this.sprite2;
+            }
     }
 
-    // void RighthandMove2()
-    // {
 
-    //     transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    // }
 
 
 }
